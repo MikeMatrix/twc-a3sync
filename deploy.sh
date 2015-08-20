fe85b2ebb510baffcd0212e51ac5d9ae28ba6e31
Michael Braun <m.braun92@gmail.com>
2015-08-20 15:58:55 +0200

    Updated upload method

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-find release -type f -exec sh -c '"$0" "$@" &' curl -u $DEPLOYMENT_FTP_USER:$DEPLOYMENT_FTP_PASS --ftp-create-dirs -T {} ftp://blacksky-dev.com/test/{} \;
-wait
-echo "Upload finished."
+ftpsync -l ./release -r test -h blacksky-dev.com -p 21 -u $DEPLOYMENT_FTP_USER -s $DEPLOYMENT_FTP_PASS -c 10
