c9b48d3cba1c0091f07be6363e440348aa21d051
Michael Braun <m.braun92@gmail.com>
2015-08-20 15:15:50 +0200

    Waiting?

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 find release -type f -exec sh -c '"$0" "$@" &' curl -u $DEPLOYMENT_FTP_USER:$DEPLOYMENT_FTP_PASS --ftp-create-dirs -T {} ftp://blacksky-dev.com/test/{} \;
+wait
+echo "Upload finished."
