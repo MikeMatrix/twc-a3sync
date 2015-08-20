adb7fe430a79533c95b6ed3af1f64fb8e69bea06
Michael Braun <m.braun92@gmail.com>
2015-08-20 15:12:44 +0200

    Yup... more tweaking

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find release -type f -exec curl -u $DEPLOYMENT_FTP_USER:$DEPLOYMENT_FTP_PASS --ftp-create-dirs -T {} ftp://blacksky-dev.com/test/{} \;
+find release -type f -exec sh -c '"$0" "$@" &' curl -u $DEPLOYMENT_FTP_USER:$DEPLOYMENT_FTP_PASS --ftp-create-dirs -T {} ftp://blacksky-dev.com/test/{} \;
